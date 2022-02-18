29d91d3aed1044c10a5d3986d5fef5d075a88ede
cosmin.dumitru <cosmindumi20@yahoo.com>
2022-02-18 15:43:28 +0200

    commit all

---
+++ MVC Cost Control/MVC Cost Control/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_Cost_Control.Models;
+
+namespace MVC_Cost_Control.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Expense> Expenses { get; set; }
+
+        public DbSet<ExpenseType> ExpenseTypes { get; set; }
+    }
+}
---
+++ MVC Cost Control/MVC Cost Control/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Cost_Control.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
